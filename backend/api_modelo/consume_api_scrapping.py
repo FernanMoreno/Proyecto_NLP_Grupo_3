ae80239ca36be5692748e69065df4f79bd66062f
maikol <mgarrido2503@gmail.com>
2023-11-14 11:37:03 +0100

    initial

---
+++ backend/api_modelo/consume_api_scrapping.py
@@ -0,0 +1,35 @@
+import requests
+
+class API_scrapping_class:
+    def __init__(self, server_url):
+        self.server_url = server_url
+
+
+    def scrap_comments(self, urls):
+        endpoint = f"{self.server_url}/api/scrap-comments"
+        data = {'urls': urls}
+        response = requests.post(endpoint, json=data)
+
+        if response.status_code == 200:
+            comentarios_data = response.json()
+            # Aquí puedes trabajar con los datos de comentarios recibidos
+            return comentarios_data
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+
+
+    def search_and_scrap_videos(self, query):
+        endpoint = f"{self.server_url}/api/search-and-scrap-videos"
+        data = {'query': query}
+        response = requests.post(endpoint, json=data)
+
+        if response.status_code == 200:
+            comentarios_data = response.json()
+            # Aquí puedes trabajar con los datos de comentarios recibidos
+            return comentarios_data
+        else:
+            print(f"Error: {response.status_code}")
+            return None
+
+
---
+++ frontend/src/app/busqueda-por-tema/busqueda-por-tema.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda-por-tema',
+  templateUrl: './busqueda-por-tema.component.html',
+  styleUrls: ['./busqueda-por-tema.component.css']
+})
+export class BusquedaPorTemaComponent {
+
+}
